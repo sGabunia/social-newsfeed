eb150ae71fff01ec136c1405ecd13ee1ae05c1aa
Sergo Gabunia <59258830+sGabunia@users.noreply.github.com>
2025-06-18 12:46:23 +0400

    Faking as user is logged in

--- src/app/routes/posts/posts.tsx
+++ src/app/routes/posts/posts.tsx
@@ -1,7 +1,18 @@
 import { CreatePost } from '@/features/posts/components/create-post';
 import { PostsList } from '@/features/posts/components/posts-list';
+import { useUser } from '@/lib/auth';
 
 const Posts = () => {
+  const user = useUser();
+  // Most likely, the user is already authenticated at this point. Faking as user already logged in.
+
+  if (user.isLoading) {
+    return <div>Initial Loading...</div>;
+  }
+
+  if (!user.data) {
+    return null;
+  }
   return (
     <div>
       <CreatePost />
