03ae6d95f4a91b50c89e4b12b1d4a33b84fd2b89
Sergo Gabunia <59258830+sGabunia@users.noreply.github.com>
2025-06-19 11:04:08 +0400

    Configure mantine

--- src/provider.tsx
+++ src/provider.tsx
@@ -3,6 +3,9 @@ import React from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { queryConfig } from '@/lib/react-query';
 
+import '@mantine/core/styles.css';
+import { MantineProvider } from '@mantine/core';
+
 export const AppProvider = ({ children }: { children: React.ReactNode }) => {
   const [queryClient] = React.useState(
     () =>
@@ -10,5 +13,9 @@ export const AppProvider = ({ children }: { children: React.ReactNode }) => {
         defaultOptions: queryConfig
       })
   );
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <MantineProvider>{children}</MantineProvider>
+    </QueryClientProvider>
+  );
 };
